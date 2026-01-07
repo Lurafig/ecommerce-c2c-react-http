f7060a33b52eb5ec2ab908caf294d66d70746522
Lurafig <Lurafigit@gmail.com>
2026-01-07 00:22:29 -0500

    navbar

---
+++ frontend/src/layouts/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import PostAdImage from "../../assets/AddA.svg";
+import ShortButton from "../../components/Buttons/ShortButton";
+const p = true;
+
+export default function Navbar() {
+  return (
+    <nav className={styles.MainNavbar}>
+      <div className={styles.HomeAndUploadCtn}>
+        <ShortButton
+          buttonID="hellou"
+          content={<img src={PostAdImage} height="50"></img>}
+        />
+      </div>
+      <div className={styles.profileLinkCtn}>{p && <p>2</p>}</div>
+    </nav>
+  );
+}
