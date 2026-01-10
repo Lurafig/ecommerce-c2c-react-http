e29297d5757dbefd98c177ff0044cc1ac49f8858
Lurafig <Lurafigit@gmail.com>
2026-01-09 23:55:50 -0500

    adslist ctn adjusts

--- frontend/src/components/AdCtn/AdCtn.jsx
+++ frontend/src/components/AdCtn/AdCtn.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+import fotoTeste from "../../assets/fotoTeste.png";
+
+import formatPrice from "../../utils/formatPrice";
+
 import "./AdCtn.css";
 
 export default function AdCtn({ id, description, price }) {
   return (
     <a key={id} className="AdCtn" href="youtube.com">
-      <img className="AdImage"></img>
+      <img src={fotoTeste} className="AdImage" draggable="false"></img>
       <div className="AdInfos">
         <h2>{description}</h2>
-        <h4>{price}</h4>
+        <h4>{formatPrice(price)}</h4>
       </div>
     </a>
   );
