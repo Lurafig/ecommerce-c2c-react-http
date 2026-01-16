27b1ed77a250873156b7cbf2e28cba3b508200cd
Lurafig <Lurafigit@gmail.com>
2026-01-16 00:04:04 -0500

    format price function adjust

--- frontend/src/utils/formatPrice.js
+++ frontend/src/utils/formatPrice.js
@@ -1,11 +1,19 @@
 export default function formatPrice(price) {
+  if (typeof price !== "number") {
+    throw new Error("O preço deve ser um valor válido");
+  }
+
+  if (price <= 0) {
+    return "Grátis";
+  }
+
   const options = {
     style: "currency",
     currency: "BRL",
     currencyDisplay: "symbol",
   };
 
-  const to = Intl.NumberFormat("pt-BR", options);
+  const to = new Intl.NumberFormat("pt-BR", options);
 
   return to.format(price);
 }
