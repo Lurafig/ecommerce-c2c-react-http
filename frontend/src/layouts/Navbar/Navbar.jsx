6cf2e261ca948616661743036aa89452199b5e40
Lurafig <Lurafigit@gmail.com>
2026-01-26 22:40:27 -0500

    navbar changes

--- frontend/src/layouts/Navbar/Navbar.jsx
+++ frontend/src/layouts/Navbar/Navbar.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import styles from "./Navbar.module.css";
+import React, { useState } from "react";
+
 import PostAdImage from "../../assets/AddA.svg";
-import ShortButton from "../../components/Buttons/ShortButton";
-const p = true;
+import NoProfilePic from "../../assets/no-profile-pic.svg";
+
+import ShortButton from "../../components/Buttons/ShortButton/ShortButton";
+
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [isOpen, setIsopen] = useState(false);
+
+  const toggleLoginDisplay = () => {
+    setIsopen(!isOpen);
+  };
+
+  const e = {
+    a: { to: "/profile", className: styles.profileLink },
+    b: { onclick: toggleLoginDisplay },
+  };
+
   return (
     <nav className={styles.MainNavbar}>
       <div className={styles.HomeAndUploadCtn}>
+        <Link to="/home" className={styles.HomeLink}>
+          <p>Home</p>
+        </Link>
         <ShortButton
+          page="/post"
           buttonID="hellou"
           content={<img src={PostAdImage} height="50"></img>}
         />
       </div>
-      <div className={styles.profileLinkCtn}>{p && <p>2</p>}</div>
+      <div className={styles.profileLinkCtn}>
+        <Link {...{ ...e.a, className: styles.profileLink }}>
+          <p>Lucas Ramos</p>
+          <ShortButton
+            page="/profile"
+            buttonID="opa"
+            content={<img src={NoProfilePic} height="50"></img>}
+            background="var(--background2)"
+          ></ShortButton>
+        </Link>
+      </div>
     </nav>
   );
 }
