f3cde90b266cae0f9ed33848e3132d5212c3a066
Lurafig <Lurafigit@gmail.com>
2026-02-21 19:37:47 -0500

    window width hook added

--- frontend/src/hooks/useAds.js
+++ frontend/src/hooks/useAds.js
@@ -5,38 +5,39 @@ const p = [];
 import formatPrice from "../utils/formatPrice";
 
 export async function loadin() {
-  return await new Promise((resolve) => {
-    let i = 0;
-    const p = setInterval(() => {
-      if (i < 2) {
-        i++;
-        return;
-      }
+  return fetch("http://localhost:3000/api/produtos");
+  // return await new Promise((resolve) => {
+  //   let i = 0;
+  //   const p = setInterval(() => {
+  //     if (i < 2) {
+  //       i++;
+  //       return;
+  //     }
 
-      const texts = [
-        "testando d ednjnrjnjnd de edndejnj denjdn jednedjed dejnjdnadjndneidniueuidheihidhaidihehdiuaehudiheuad adniuheadhuaehduh adnaeuhduiaehui eaduneidhiuahefuuyrygyg beduyysygsyugf",
-        "opa eu sou iiai dna dnudenu iaa iaa ie ie ie ie",
-        "eu não sei",
-      ];
+  //     const texts = [
+  //       "testando d ednjnrjnjnd de edndejnj denjdn jednedjed dejnjdnadjndneidniueuidheihidhaidihehdiuaehudiheuad adniuheadhuaehduh adnaeuhduiaehui eaduneidhiuahefuuyrygyg beduyysygsyugf",
+  //       "opa eu sou iiai dna dnudenu iaa iaa ie ie ie ie",
+  //       "eu não sei",
+  //     ];
 
-      resolve(
-        (() => {
-          let list = [];
-          for (let i = 0; i < 15; i++) {
-            list.push({
-              id: i,
-              description: texts[Math.floor(Math.random() * 3)],
-              price: formatPrice(parseFloat((Math.random() * 10).toFixed(2))),
-            });
-          }
-          return list;
-        })(),
-      );
+  //     resolve(
+  //       (() => {
+  //         let list = [];
+  //         for (let i = 0; i < 15; i++) {
+  //           list.push({
+  //             id: i,
+  //             description: texts[Math.floor(Math.random() * 3)],
+  //             price: formatPrice(parseFloat((Math.random() * 10).toFixed(2))),
+  //           });
+  //         }
+  //         return list;
+  //       })(),
+  //     );
 
-      clearInterval(p);
-      return;
-    }, 1000);
-  });
+  //     clearInterval(p);
+  //     return;
+  //   }, 1000);
+  // });
 }
 
 export function useAds() {
@@ -46,7 +47,7 @@ export function useAds() {
 
   useEffect(() => {
     loadin()
-      .then((res) => res)
+      .then((res) => res.json())
       .then((res) => {
         setAds(res);
         console.log(res);
